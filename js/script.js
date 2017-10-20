105b646bc9870c911345eca3150bc8bd10314f36
Karin Christensen <karin.christensen@chasacademy.se>
2017-10-20 19:10:50 +0200

    letter buttons and added my own letter boxes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
 // Globala variabler
 
-var wordList; // Lista med spelets alla ord
-var selectedWord; // Ett av orden valt av en slumpgenerator
+var wordList = ['björk', 'gröt', 'broms', 'yxa', 'monster', 'flagga'];
+var selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
 var letterBoxes; //Rutorna där bokstäverna ska stå
 var hangmanImg; //Bild som kommer vid fel svar
 var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
 var msgElem; // Ger meddelande när spelet är över
-var startGameBtn; // Knappen du startar spelet med
-var letterButtons; // Knapparna för bokstäverna
+var startGameBtn = document.getElementById('startGameBtn'); // Knappen du startar spelet med
+var letterButtons = document.querySelectorAll('#letterButtons > li > button');
 var startTime; // Mäter tiden
 
-// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
-// Initiering av globala variabler samt koppling av funktioner till knapparna.
-function init() {} // End init
 
-window.onload = init; // Se till att init aktiveras då sidan är inladdad
+function addLetterListeners() {
+    for (var i = 0; i < letterButtons.length; i++){
+      letterButtons[i].addEventListener('click', letterWasClicked);
+    }
+}  
+    
+    
+function letterWasClicked() {
+    
+    const letterThatWasClicked = this.value;
+    this.disabled = true;
+    
+    console.log(letterThatWasClicked);
+}
+addLetterListeners();
+
+
+function getLetterBoxes() {
+        
+    let box;
+    
+    box= ""
+    
+    for (var j = 0; j < selectedWord.length; j++) {
+    
+        box += '<input>&nbsp;</input>';
+      
+    }
+    document.getElementById('letterBoxes').innerHTML = box;
+    letterBoxes = document.getElementById('letterBoxes').getElementsByTagName('input');
+}
+    console.log(getLetterBoxes);
+    console.log(selectedWord);
+getLetterBoxes();
+
+
+// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
+
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 
